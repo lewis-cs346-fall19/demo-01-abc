ef88312157199c395f37d4498b39cc1e6fa496bd
Augustine Xu <31711513+augustinexzy@users.noreply.github.com>
2019-09-03 09:51:33 -0700

    server

---
+++ net_server.py
@@ -0,0 +1,13 @@
+import socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+addr = ("0.0.0.0", 10101)
+sock.bind(addr)
+sock.listen(5)
+while True:
+    (connectedSock,clientAddress) = sock.accept()
+    msg = connectedSock.recv(1024).decode()
+    msg = "Here's the data" + msg
+    sock.close()
+
+sock.sendall(msg.encode())
+
